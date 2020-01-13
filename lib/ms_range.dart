6908addcec2fd3547eed11c053f814d089ca8ac8
Marcus Duarte <marcusedu7@gmail.com>
2020-01-13 08:22:12 -0200

    removed unnecessary use 'this.'

--- lib/ms_range.dart
+++ lib/ms_range.dart
@@ -11,17 +11,17 @@ class Range {
   /// [start] number that start a range
   /// [end] number that end a range
   Range(num start, num end, [num step = 1])
-      : this.start = math.min(start, end),
-        this.end = math.max(start, end),
-        this.step = step;
+      : start = math.min(start, end),
+        end = math.max(start, end),
+        step = step;
 
   /// return true if [numToTest] is major or equal [start] or minor or equal [end]
   bool isInRange(num numToTest) =>
-      numToTest >= this.start && numToTest <= this.end;
+      numToTest >= start && numToTest <= end;
 
   /// return true if [numToTest] is major [start] or minor [end]
   bool isBetween(num numToTest) =>
-      numToTest > this.start && numToTest < this.end;
+      numToTest > start && numToTest < end;
 
   /// return a Interable for range [start]..[end]
   Iterable<num> get interate sync* {
@@ -41,22 +41,22 @@ class Range {
   bool operator ==(Object other) {
     if (other is num) return isInRange(other);
     if (other is Range) {
-      return other.start == this.start && other.end == this.end;
+      return other.start == start && other.end == end;
     }
     return false;
   }
 
   /// check if other number is major that range
   bool operator >(Object other) {
-    if (other is num) return (other) > this.end;
-    if (other is Range) return other.end > this.end;
+    if (other is num) return (other) > end;
+    if (other is Range) return other.end > end;
     return false;
   }
 
   /// check if other number is minor that range
   bool operator <(Object other) {
-    if (other is num) return other < this.start;
-    if (other is Range) return other.start > this.start;
+    if (other is num) return other < start;
+    if (other is Range) return other.start > start;
     return false;
   }
 }
